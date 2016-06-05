d8fa5166fad74878fd3c0af125f6d5b107294b54
James Smyth <smythie_89@hotmail.com>
2016-06-05 12:19:21 +0100

    filtering on both type and style now implemented

--- containers/BeersContainer.jsx
+++ containers/BeersContainer.jsx
@@ -9,55 +9,74 @@ class BeersContainerView extends React.Component {
         this.state = {
             types: this.props.types,
             styles: this.props.styles,
-            type: ['all'],
-            style: ['all']
+            type: [],
+            style: []
         }
     }
 
     handleTypeClick (type) {
-        console.log(type.type)
-        console.log(this.state.type)
         var types = this.state.type;
-        types.push(type.type);
+        var isPresent = 0;
 
-        console.log(types);
+        // looping through the currently selected types array and if the passed 'type' is in the list it is removed. Else added.
+        for(var i in types) {
+            if(type.type === types[i]) {
+                isPresent++;
+                types.splice(i, 1);
+            }
+        }
+
+        if(isPresent === 0) {
+            types.push(type.type);
+        }
+
         this.setState({"type": types });
     }
 
     handleStyleClick (style) {
-        this.setState({ "style": event.target.value });
+        var styles = this.state.style;
+        var isPresent = 0;
+
+        // looping through the currently selected styles array and if the passed 'style' is in the list it is removed. Else added.
+        for(var i in styles) {
+            if(style.style === styles[i]) {
+                isPresent++;
+                styles.splice(i, 1);
+            }
+        }
+
+        if(isPresent === 0) {
+            styles.push(style.style);
+        }
+
+        this.setState({"style": styles });
     }
 
     render () {
         var beers = this.props.beers;
         var types = this.state.types;
         var styles = this.state.styles;
-        var type = this.state.type;
-        var style = this.state.style;
+        var selectedType = this.state.type;
+        var selectedStyle = this.state.style;
         var handleTypeSelect = this.handleTypeClick.bind(this);
         var handleStyleSelect = this.handleStyleClick.bind(this);
 
-        // console.log(this.state.styles);
-        // console.log(this.state.types);
-
         // creating the toggle tabs for the beer types
         var typeOptions = types.map(function (type, i) {
-            // var newClass = borough === stateBorough ? 'selected' : null
-            return <li key={i} onClick={() => handleTypeSelect({type})}>{type}</li>
+            var typeClass = selectedType.indexOf(type) > -1 ? 'selected' : null;
+            return <li key={i} className={typeClass} onClick={() => handleTypeSelect({type})}>{type}</li>
         });
 
         // creating the toggle tabs for the beer styles
         var styleOptions = styles.map(function (style, i) {
-            // var newClass = borough === stateBorough ? 'selected' : null
-            return <li key={i} onClick={() => handleStyleSelect({style})}>{style}</li>
+            var styleClass = selectedStyle.indexOf(style) > -1 ? 'selected' : null;
+            return <li key={i} className={styleClass} onClick={() => handleStyleSelect({style})}>{style}</li>
         });
 
+        // filtering out the beers by checking if the selected tabs are indexed in each of the beers properties
         var beerList = Object.keys(beers).map(function (beer, i) {
-
-            // if((beers[beer].type == type) || (type == 'all')) {
-
-                // if((beers[beer].style == style) || (style == 'all')) {
-
+            if((selectedType.indexOf(beers[beer].type) > -1) || (selectedType.length === 0)) {
+                if((selectedStyle.indexOf(beers[beer].style) > -1) || (selectedStyle.length === 0)) {
                     return <li key={i}>
                                 <h3>
                                     <Link to={"/beers/" + beer}>
@@ -68,25 +87,30 @@ class BeersContainerView extends React.Component {
                                     <img src={beers[beer].photo} alt={beers[beer].name} className="beer-image" />
                                 </Link>
                             </li>
-                // }
-            // }
+                }
+
+            }
         });
 
         return (
             <div>
                 {!this.props.children ?
-                    <section className="split">
-                        <h1>Beers</h1>
-                        <ul className="tabs-list">
-                            {typeOptions}
-                        </ul>
-                        <ul className="tabs-list">
-                            {styleOptions}
-                        </ul>
-                        <ul className="beers-list">
-                            {beerList}
-                        </ul>
-                    </section>
+                    <div>
+                        <section className="split">
+                            <h1>Beers</h1>
+                            <ul className="tabs-list">
+                                {typeOptions}
+                            </ul>
+                            <ul className="tabs-list">
+                                {styleOptions}
+                            </ul>
+                        </section>
+                        <section className="split">
+                            <ul className="beers-list">
+                                {beerList}
+                            </ul>
+                        </section>
+                    </div>
                 : null}
                 {this.props.children}
             </div>
