333b6ce43012b7b1cf1cf4d0792fe64b699544de
James Smyth <smythie_89@hotmail.com>
2016-07-27 16:45:27 +0100

    updated reducers that do not need to create newStates

--- reducers/BeerStylesReducer.jsx
+++ reducers/BeerStylesReducer.jsx
@@ -1,19 +1,7 @@
 import Data from '../data/data.js'
 
 const BeerStylesReducer = (state = Data.beerStyles, action) => {
-
-    var newState = Object.assign({}, state);
-
-    switch (action.type) {
-
-        case 'ALL_BEER_STYLES':
-            newState = action.data.beerStyles;
-            return newState;
-            break;
-
-        default:
-            return state;
-    }
+    return state;
 }
 
 export default BeerStylesReducer
--- reducers/BeerTypesReducer.jsx
+++ reducers/BeerTypesReducer.jsx
@@ -1,19 +1,7 @@
 import Data from '../data/data.js'
 
 const BeerTypesReducer = (state = Data.beerTypes, action) => {
-
-    var newState = Object.assign({}, state);
-
-    switch (action.type) {
-        case 'ALL_BEER_TYPES':
-
-            newState = action.data.beerTypes;
-            return newState;
-            break;
-
-        default:
-            return state;
-    }
+    return state;
 }
 
 export default BeerTypesReducer
