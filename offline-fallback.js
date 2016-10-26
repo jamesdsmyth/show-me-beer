050c5cf8964907f800729d289aa3a9ce5e31e5e2
James Smyth <smythie_89@hotmail.com>
2016-10-26 17:35:01 +0100

    Removed unneeded service worker code

--- offline-fallback.js
+++ offline-fallback.js
@@ -20,20 +20,20 @@ if (navigator.serviceWorker.controller) {
 }
 
 // The refresh link needs a cache-busting URL parameter
-document.querySelector('#refresh').search = Date.now();
+// document.querySelector('#refresh').search = Date.now();
 
 // Debug helper
 function debug(message, element, append) {
-  var target = document.querySelector('#' + (element || 'log'));
-  target.textContent = message + ((append) ? ('/n' + target.textContent) : '');
+  // var target = document.querySelector('#' + (element || 'log'));
+  // target.textContent = message + ((append) ? ('/n' + target.textContent) : '');
 }
 
-// Allow for "replaying" this example
-document.getElementById('clearAndReRegister').addEventListener('click',
-  function() {
-    navigator.serviceWorker.getRegistration().then(function(registration) {
-      registration.unregister();
-      window.location.reload();
-    });
-  }
-);
+// // Allow for "replaying" this example
+// document.getElementById('clearAndReRegister').addEventListener('click',
+//   function() {
+//     navigator.serviceWorker.getRegistration().then(function(registration) {
+//       registration.unregister();
+//       window.location.reload();
+//     });
+//   }
+// );
